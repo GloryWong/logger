23c2cccea6d72bdc7069a46e41803b9c015867dd
GloryWong <glorywong1001@gmail.com>
2024-07-12 16:53:32 +0800

    chore(deps): upgrade deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,4 @@
 import antfu from '@antfu/eslint-config'
-import perfectionist from 'eslint-plugin-perfectionist'
 
 // https://eslint.vuejs.org/rules/
-export default await antfu({
-  plugins: {
-    perfectionist,
-  },
-})
+export default await antfu()
